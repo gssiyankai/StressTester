f805ae52748263f110dde341ce61f28f6777e5b2
gssiyankai <syk.greg@gmail.com>
2016-04-08 16:04:02 +0200

    Implement stress load testing

--- src/main/java/com/gregory/testing/run/TestRunner.java
+++ src/main/java/com/gregory/testing/run/TestRunner.java
@@ -7,6 +7,10 @@ import com.gregory.testing.result.BatchResult;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public final class TestRunner {
 
@@ -22,6 +26,8 @@ public final class TestRunner {
                 return runConstantLoad();
             case INCREMENTAL_LOAD:
                 return runIncrementalLoad();
+            case STRESS_LOAD:
+                return runStressLoad();
             default:
                 throw new RuntimeException("Strategy " + testCase.strategy().name() + " is not supported yet!");
         }
@@ -53,6 +59,30 @@ public final class TestRunner {
             batchSize++;
             BatchTask task = new BatchTask(batchId++, server, batchMessages);
             results.add(task.call());
+        }
+        return results;
+    }
+
+    private List<BatchResult> runStressLoad() throws ExecutionException, InterruptedException {
+        int numberOfThreads = 16;
+
+        Server server = testCase.server();
+        List<Message> messages = testCase.messages();
+        List<BatchTask> tasks = new ArrayList<>();
+        int batchId = 0;
+        for (List<Message> batchMessages : Lists.partition(messages, messages.size()/numberOfThreads)) {
+            BatchTask task = new BatchTask(batchId++, server, batchMessages);
+            tasks.add(task);
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+        List<Future<BatchResult>> futures = new ArrayList<>(tasks.size());
+        for (BatchTask task : tasks) {
+            futures.add(executorService.submit(task));
+        }
+        List<BatchResult> results = new ArrayList<>();
+        for (Future<BatchResult> future : futures) {
+            results.add(future.get());
         }
         return results;
     }
--- src/main/java/com/gregory/testing/strategy/TestingStrategy.java
+++ src/main/java/com/gregory/testing/strategy/TestingStrategy.java
@@ -1,5 +1,5 @@
 package com.gregory.testing.strategy;
 
 public enum TestingStrategy {
-    CONSTANT_LOAD, INCREMENTAL_LOAD, STRESS
+    CONSTANT_LOAD, INCREMENTAL_LOAD, STRESS_LOAD
 }
