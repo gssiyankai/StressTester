806c01f4bb851aa418e4b1e00a0f4b71fb84e3f6
gssiyankai <syk.greg@gmail.com>
2016-04-08 14:16:55 +0200

    Implement constant load run

---
+++ src/main/java/com/gregory/testing/run/TestRunner.java
@@ -0,0 +1,44 @@
+package com.gregory.testing.run;
+
+import com.gregory.testing.application.Server;
+import com.gregory.testing.communication.InputChannel;
+import com.gregory.testing.communication.OutputChannel;
+import com.gregory.testing.message.Message;
+import com.gregory.testing.message.TimestampedMessage;
+import com.gregory.testing.result.Result;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class TestRunner {
+
+    private final TestCase testCase;
+
+    public TestRunner(TestCase testCase) {
+        this.testCase = testCase;
+    }
+
+    public void run() throws Exception {
+        switch (testCase.strategy()) {
+            case CONSTANT_LOAD:
+                runConstantLoad();
+                break;
+            default:
+                throw new RuntimeException("Strategy " + testCase.strategy().name() + " is not supported yet!");
+        }
+    }
+
+    private void runConstantLoad() throws IOException {
+        List<Result> results = new ArrayList<>();
+        Server server = testCase.server();
+        InputChannel input = server.input();
+        OutputChannel output = server.output();
+        for (Message message : testCase.messages()) {
+            TimestampedMessage request = input.sendMessaage(message);
+            TimestampedMessage response = output.getMessage();
+            results.add(new Result(request, response));
+        }
+    }
+
+}
