7860fdb7f181397ae53a1e6b9a3a4d7f5068cb62
gssiyankai <syk.greg@gmail.com>
2016-04-08 15:47:49 +0200

    Implement incremental load test

--- src/main/java/com/gregory/testing/run/TestRunner.java
+++ src/main/java/com/gregory/testing/run/TestRunner.java
@@ -20,6 +20,8 @@ public final class TestRunner {
         switch (testCase.strategy()) {
             case CONSTANT_LOAD:
                 return runConstantLoad();
+            case INCREMENTAL_LOAD:
+                return runIncrementalLoad();
             default:
                 throw new RuntimeException("Strategy " + testCase.strategy().name() + " is not supported yet!");
         }
@@ -32,6 +34,23 @@ public final class TestRunner {
         List<BatchResult> results = new ArrayList<>();
         int batchId = 0;
         for (List<Message> batchMessages : Lists.partition(messages, batchSize)) {
+            BatchTask task = new BatchTask(batchId++, server, batchMessages);
+            results.add(task.call());
+        }
+        return results;
+    }
+
+    private List<BatchResult> runIncrementalLoad() throws Exception {
+        int batchSize = testCase.initialBatchSize();
+        Server server = testCase.server();
+        List<Message> messages = testCase.messages();
+        List<BatchResult> results = new ArrayList<>();
+        int batchId = 0;
+        int messageIndex = 0;
+        while(messageIndex < messages.size()) {
+            List<Message> batchMessages = messages.subList(messageIndex, Math.min(messages.size(), messageIndex + batchSize));
+            messageIndex += batchSize;
+            batchSize++;
             BatchTask task = new BatchTask(batchId++, server, batchMessages);
             results.add(task.call());
         }
