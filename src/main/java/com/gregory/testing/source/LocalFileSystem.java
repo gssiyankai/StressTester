41137b6bfafb450989dc925509420cde94cb6cd6
gssiyankai <syk.greg@gmail.com>
2016-04-08 13:44:37 +0200

    Add source to generate messages from local fs

---
+++ src/main/java/com/gregory/testing/source/LocalFileSystem.java
@@ -0,0 +1,39 @@
+package com.gregory.testing.source;
+
+import com.gregory.testing.message.Message;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public final class LocalFileSystem {
+
+    public static List<Message> messagesFromPath(String path) throws FileNotFoundException {
+        List<Message> messages = new ArrayList<>();
+        File file = new File(path);
+        if(file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                messages.addAll(messagesFromPath(f.getPath()));
+            }
+        } else {
+            try (Scanner sc = new Scanner(file)) {
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    messages.add(new Message(line.getBytes()));
+                }
+            }
+        }
+        return messages;
+    }
+
+    public static List<Message> messagesFromPath(String... paths) throws FileNotFoundException {
+        List<Message> messages = new ArrayList<>();
+        for (String path : paths) {
+            messages.addAll(messagesFromPath(path));
+        }
+        return messages;
+    }
+
+}
