79bdf4b33e0eb5d75423678b7641334eaf74fe63
gssiyankai <syk.greg@gmail.com>
2016-04-09 20:54:49 +0200

    Add test

--- src/main/java/com/gregory/testing/log/Logger.java
+++ src/main/java/com/gregory/testing/log/Logger.java
@@ -3,10 +3,7 @@ package com.gregory.testing.log;
 import com.gregory.testing.message.TimestampedMessage;
 import com.gregory.testing.result.BatchResult;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.List;
 
 import static com.gregory.testing.utils.Utils.join;
@@ -17,24 +14,28 @@ public final class Logger {
     }
 
     public static void saveAsCsv(String path, String separator, List<BatchResult> results) throws IOException {
-        try (FileWriter fw = new FileWriter(path + ".csv");
+        File file = new File(path);
+        file.getParentFile().mkdirs();
+        try (FileWriter fw = new FileWriter(file);
              BufferedWriter bw = new BufferedWriter(fw);
              PrintWriter out = new PrintWriter(bw)) {
             for (BatchResult result : results) {
                 List<TimestampedMessage> requests = result.requests();
                 for (TimestampedMessage request : requests) {
                     out.append(messageToRow(result.id(), "request", request, separator));
+                    out.append("\n");
                 }
                 List<TimestampedMessage> responses = result.responses();
                 for (TimestampedMessage response : responses) {
                     out.append(messageToRow(result.id(), "response", response, separator));
+                    out.append("\n");
                 }
             }
         }
     }
 
     private static String messageToRow(int batchId, String messageType, TimestampedMessage message, String separator) {
-        return join(separator, batchId, messageType, new String(message.message().data()));
+        return join(separator, batchId, messageType, message.timestamp(), new String(message.message().data()));
     }
 
 }
---
+++ src/test/java/com/gregory/testing/log/LoggerTest.java
@@ -0,0 +1,32 @@
+package com.gregory.testing.log;
+
+import com.gregory.testing.message.Message;
+import com.gregory.testing.message.TimestampedMessage;
+import com.gregory.testing.result.BatchResult;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+import static com.gregory.testing.log.Logger.saveAsCsv;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class LoggerTest {
+
+    @Test
+    public void it_should_save_results_as_csv() throws IOException {
+        TimestampedMessage request = new TimestampedMessage(100, new Message("Hello".getBytes()));
+        TimestampedMessage response = new TimestampedMessage(101, new Message("Hi".getBytes()));
+        BatchResult result = new BatchResult(0, Collections.singletonList(request), Collections.singletonList(response));
+        File actualResultFile = new File("target/LoggerTest/results.csv");
+        File expectedResultFile = new File("src/test/resources/results/results.txt");
+        if (actualResultFile.exists()) {
+            actualResultFile.delete();
+        }
+        assertThat(actualResultFile).doesNotExist();
+        saveAsCsv(actualResultFile.getPath(), "\t", Collections.singletonList(result));
+        assertThat(actualResultFile).exists().hasSameContentAs(expectedResultFile);
+    }
+
+}
